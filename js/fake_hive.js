7b9f802e7805b1a1b2ee777991d20fa5c591947c
Dariusz Walczak <walczak.darek@gmail.com>
2013-10-17 14:56:59 +0200

    new API

--- js/fake_hive.js
+++ js/fake_hive.js
@@ -6,26 +6,42 @@
  * To change this template use File | Settings | File Templates.
  */
 var bitcoin = bitcoin || {
-    getClientInfo: function (callback) {
-        var info = {
-            firstname: 'Homer',
-            lastname:  'Simpson',
+    BTC_IN_SATOSHI: 100000000,
+    MBTC_IN_SATOSHI: 100000,
+    UBTC_IN_SATOSHI: 100,
+
+    sendMoney: function(hash, amount, callback){
+        if (!hash){
+            throw "hash argument is undefined";
+        }
+        if (callback){
+            callback(true, 'FAKE_HASH');
+        }
+    },
+
+    getTransaction: function(id, callback){
+        if (!id || !callback){
+            throw "id and callback are required";
+        }
+        callback({
+            id: 123,
+            amount: 10,
+            received: true,
+            timestamp: new Date(),
+            inputAddresses: ['HASH1'],
+            outputAddresses: ['HASH2']
+        });
+    },
+
+    getUserInfo: function(callback){
+        if (!callback){
+            throw "callback is required";
+        }
+        callback({
+            firstName: 'Homer',
+            lastName:  'Simpson',
             email:     'homer@fake.com',
             address:   'poqjer23rfc234laq',
-            street:    'next to Flanders',
-            zipcode:   '12233',
-            city:      'Springfield',
-            country:   'USA'
-        };
-
-        return callback(info);
-    },
-
-    sendCoins: function (hash, amount, callback) {
-        return callback(true, hash);
-    },
-
-    sendCoinsForAddress: function(hash, amount, callback) {
-        return callback(true, hash);
+        });
     }
 };
